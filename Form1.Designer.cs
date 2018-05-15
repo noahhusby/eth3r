0eff756e6800e8ed22e17ba944989b7736e4f6cb
TKO-Cuber <tko.cuber@gmail.com>
2018-05-14 21:46:46 -0400

    Fix up generation Form... add checks to make sure everything is filled out correctly... next step is to add the actual IPSW generation process

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GenerateFirmware));
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -38,6 +39,9 @@
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.label4 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,7 +50,7 @@
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label1.Font = new System.Drawing.Font("Tahoma", 19.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Lime;
-            this.label1.Location = new System.Drawing.Point(197, 31);
+            this.label1.Location = new System.Drawing.Point(329, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(505, 53);
             this.label1.TabIndex = 0;
@@ -57,18 +61,17 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "iPhone2,1"});
-            this.comboBox1.Location = new System.Drawing.Point(138, 202);
+            this.comboBox1.Location = new System.Drawing.Point(129, 202);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(216, 24);
             this.comboBox1.TabIndex = 1;
-            this.comboBox1.Text = "Select a Device";
             // 
             // label2
             // 
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label2.Font = new System.Drawing.Font("Tahoma", 12.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
             this.label2.ForeColor = System.Drawing.Color.Lime;
-            this.label2.Location = new System.Drawing.Point(197, 150);
+            this.label2.Location = new System.Drawing.Point(186, 150);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 32);
             this.label2.TabIndex = 2;
@@ -79,7 +82,7 @@
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label3.Font = new System.Drawing.Font("Tahoma", 12.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
             this.label3.ForeColor = System.Drawing.Color.Lime;
-            this.label3.Location = new System.Drawing.Point(607, 150);
+            this.label3.Location = new System.Drawing.Point(858, 150);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(95, 32);
             this.label3.TabIndex = 3;
@@ -90,11 +93,10 @@
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Items.AddRange(new object[] {
             "6.1.3"});
-            this.comboBox2.Location = new System.Drawing.Point(547, 202);
+            this.comboBox2.Location = new System.Drawing.Point(802, 202);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(216, 24);
             this.comboBox2.TabIndex = 4;
-            this.comboBox2.Text = "Select a Version";
             // 
             // checkBox1
             // 
@@ -116,7 +118,7 @@
             this.panel1.Controls.Add(this.checkBox2);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.checkBox1);
-            this.panel1.Location = new System.Drawing.Point(321, 247);
+            this.panel1.Location = new System.Drawing.Point(457, 302);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(258, 142);
             this.panel1.TabIndex = 6;
@@ -154,7 +156,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.Font = new System.Drawing.Font("Tahoma", 13F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
             this.button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.button2.Location = new System.Drawing.Point(301, 405);
+            this.button2.Location = new System.Drawing.Point(426, 472);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(299, 64);
             this.button2.TabIndex = 7;
@@ -162,12 +164,42 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 12.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(475, 122);
+            this.label5.Margin = new System.Windows.Forms.Padding(3);
+            this.label5.Name = "label5";
+            this.label5.Padding = new System.Windows.Forms.Padding(3);
+            this.label5.Size = new System.Drawing.Size(223, 60);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Select Sn0wbreeze IPSW";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(475, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 36);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Find IPSW";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // GenerateFirmware
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowText;
-            this.ClientSize = new System.Drawing.Size(921, 497);
+            this.ClientSize = new System.Drawing.Size(1130, 623);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.comboBox2);
@@ -175,6 +207,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "GenerateFirmware";
             this.Text = "Generate Firmware";
             this.panel1.ResumeLayout(false);
@@ -195,5 +228,8 @@
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -12,24 +12,61 @@ namespace Eth3r
 {
     public partial class GenerateFirmware : Form
     {
+        private bool ipswWasPressed;
+        private DialogResult dialogSelection;
+        private bool goodToGo;
+
         public GenerateFirmware()
         {
             InitializeComponent();
         }
 
+        public void button1_Click_1(object sender, EventArgs e)
+        {
+            ipswWasPressed = true; //set this for later during the ipsw generation
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "iPhone Software Files (IPSW)|*.ipsw";
+            openFileDialog1.Title = "Select your Sn0wbreeze IPSW";
+            DialogResult dialogSelection = openFileDialog1.ShowDialog();
+            string sn0Path = openFileDialog1.FileName;
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
-            //Check everything is filled out
-            if (comboBox1.SelectedIndex < -1)
-                {
+
+            //here comes the real stuff
+
+
+            //Check everything is filled out/selected
+            if (string.IsNullOrEmpty(comboBox1.Text))
+            {
                 MessageBox.Show("You did not select an item in the 'Device' dropdown list.");
+                goodToGo = false;
             }
-            if (comboBox2.SelectedIndex < -1)
+            if (string.IsNullOrEmpty(comboBox2.Text))
             {
                 MessageBox.Show("You did not select an item in the 'Version' dropdown list.");
+                goodToGo = false;
+            }
+            if (ipswWasPressed == false)
+            {
+                MessageBox.Show("You did not select a Sn0wbeeze IPSW!");
+                goodToGo = false;
+            }
+            if (dialogSelection == DialogResult.Cancel)
+            {
+                MessageBox.Show("You pressed the select IPSW button, but did not select an IPSW!");
+                goodToGo = false;
             }
 
-            //Start generating the IPSW
+            //Start generating the IPSW if we are good to go
+
+            if (goodToGo == true)
+            {
+                //we are good to go, so let's generate the ipsw, shall we?
+
+                
+            }
 
         }
     }
